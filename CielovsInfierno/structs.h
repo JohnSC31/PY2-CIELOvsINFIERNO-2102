5282305714a17e139d93dc6399a30a6bbdcaf1ce
John Sanchez <jostsace@estudiantec.cr>
2021-10-31 20:12:58 -0600

    Archivo comentado para las estructuras del proyecto

---
+++ CielovsInfierno/structs.h
@@ -0,0 +1,23 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+
+// Definicion de las todas las estructuras para el proyecto
+
+
+// -------------------------------------- PROTOTIPOS -----------------------------------------------//
+
+
+// -------------------------------------- ESTRUCTURAS DEL MUNDO ------------------------------------ //
+
+
+// -------------------------------------- ESTRUCTURAS PARA EL INFIERNO ----------------------------- //
+
+
+// -------------------------------------- ESTRUCTURAS PARA EL CIELO -------------------------------- //
+
+
+
+
+
+#endif // STRUCTS_H
