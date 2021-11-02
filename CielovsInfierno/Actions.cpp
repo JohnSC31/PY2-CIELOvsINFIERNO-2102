c1ffc8e141e0c579aee93f12d977996cf42e427d
Axel <axelchaves.r@estudiantec.cr>
2021-11-01 23:39:07 -0600

    Ahora sí funciona definitivamente el sistema de randoms

--- CielovsInfierno/Actions.cpp
+++ CielovsInfierno/Actions.cpp
@@ -1,38 +1,42 @@
 #include "structs.h"
 #include <QDebug>
-void Actions::addGoods(){
-    //int num = 1+rand()%(7);
+#include <time.h> //Librería para los random
+
+void Actions::addGoods(){ //Añade las buenas acciones al humano
+     srand(time(NULL)); //Inicializa los random
+    int randomGood;
     for(int i = 0; i < 7; i++){
-        int randomGood = rand()%(7); // Elige un índice random para colocar la cantidad de buenas acciones, es decir, escoge la buena acción a aumentar.
-        goods[randomGood] += 1+rand()%(100);
+        randomGood = rand()%(7); // Elige un índice random para colocar la cantidad de buenas acciones, es decir, escoge la buena acción a aumentar.
+        goods[randomGood] += 1+rand()%(100+1);
 
     }
 }
-void Actions::addSins(){
+void Actions::addSins(){ //Añade los pecados al humano
+    int randomGood;
     for(int i = 0; i < 7; i++){
-        int randomGood = rand()%(7); // Elige un índice random para colocar la cantidad de pecados, es decir, escoge el pecado a aumentar.
-        sinsCommited[randomGood] += 1+rand()%(100);
+        randomGood = rand()%(7); // Elige un índice random para colocar la cantidad de pecados, es decir, escoge el pecado a aumentar.
+        sinsCommited[randomGood] += 1+rand()%(100+1);
 
     }
 }
-void Actions::printGoods(){
+void Actions::printGoods(){ //Imprimir las buenas acciones
     for(int i = 0; i < 7; i++){
         qDebug() << "Buena acción: " << goodActions[i] << " Cantidad cometida: " << goods[i] << "\n";
     }
 }
-void Actions::printSins(){
+void Actions::printSins(){ //Imprime los pecados
     for(int i = 0; i < 7; i++){
-        qDebug() << "Pecado: " << sins[i] << " Cantidad cometida: " << sinsCommited[i] << "\n";
+        qDebug() << "Pecado: " << sins[i] << " Cantidad cometida: " << sinsCommited[i] << "\n"; // Recorre los subíndices para imprimir
     }
 }
-int Actions::countGoods(){
+int Actions::countGoods(){ //Contar las buenas acciones. Creo que este método puede servir después para las consultas y la salvación
     int total = 0;
     for(int i = 0; i < 7; i++){
         total += goods[i];
     }
     return total;
 }
-int Actions::countSins(){
+int Actions::countSins(){ //Contador de pecados
     int total = 0;
     for(int i = 0; i < 7; i++){
         total += sinsCommited[i];
